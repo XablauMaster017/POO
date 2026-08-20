307e83e94c506f7aa86016b2f6b30bf76c1992ed
Xablau017 <fernandomarti017@gmail.com>
2026-08-20 08:43:23 -0300

    Commiting

---
+++ Lista3/src/br/com/Lista3/model/Carro.java
@@ -0,0 +1,11 @@
+  package br.com.Lista3.model; 
+ 
+   public class Carro { 
+       public String modelo; 
+       public String cor; 
+ 
+       public void buzinar() { 
+           System.out.println("O " + modelo + " " + cor + " fez: 
+BIIII BIIII!"); 
+       } 
+   } 
